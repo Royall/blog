5f15ab601087842601486aec93033f496d938c6c
Royall <Royall@yangzblog.com>
2013-11-01 22:22:44 +0800

    Initial commit

---
+++ models/db.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 13-10-24.
+ */
+var settings = require('../settings'),
+    Db = require('mongodb').Db,
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
+module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}));
---
+++ settings.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 13-10-24.
+ */
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost'
+};
