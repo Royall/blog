6b859216f20133e1388b4c507edcaa14fb75949f
Royall <royal@yangzblog.com>
2013-11-12 10:02:07 +0800

    all

--- models/db.js
+++ models/db.js
@@ -1,8 +1,5 @@
-/**
- * Created by Administrator on 13-10-24.
- */
 var settings = require('../settings'),
     Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
-module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}));
+module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT), {safe: true});
--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
-/**
- * Created by Administrator on 13-10-24.
- */
-module.exports = {
-    cookieSecret: 'myblog',
-    db: 'blog',
-    host: 'localhost'
+module.exports = { 
+  cookieSecret: 'myblog', 
+  db: 'blog', 
+  host: 'localhost'
 };
